9a80d432a51f16c313eecdbf1c783bdd39973a4c
ritvikmathur1999 <ritvikmathur1999@gmail.com>
2021-04-19 10:21:55 +0530

    New Dir Changes

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 var app = express();
 
 app.use(logger('dev'));
-app.use(express.json());
+app.use(express.json())
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public-flutter')));
